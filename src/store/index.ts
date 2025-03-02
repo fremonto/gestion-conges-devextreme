cdb27d5983eec30fad8ec69c50b81a87d8193404
Olivier Frémont <86167108+fremonto@users.noreply.github.com>
2025-03-02 16:46:11 +0100

    Mise à jour du store avec le reducer admin

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import congesReducer from './conges/congesSlice';
+import adminReducer from './admin/adminSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     conges: congesReducer,
+    admin: adminReducer,
   },
 });
 
